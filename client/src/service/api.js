235ed2c02d1df3dd79598245aa87ae03b2555372
Dinesh Wadhwa <dinesh.wadhwa@capanicus.com>
2024-08-13 16:30:01 +0530

    url update

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'http://localhost:8000';
+const url = "https://whatsapp-clone-k5d4.onrender.com";
+// const url = 'http://localhost:8000';
 
 export const addUser = async (data) => {
   try {
@@ -8,7 +9,7 @@ export const addUser = async (data) => {
   } catch (error) {
     console.log(error.message);
   }
-}
+};
 export const getUsers = async () => {
   try {
     const response = await axios.get(`${url}/users`);
@@ -16,41 +17,41 @@ export const getUsers = async () => {
   } catch (error) {
     console.log(error.message);
   }
-}
+};
 export const setConversation = async (data) => {
   try {
     await axios.post(`${url}/conversation/add`, data);
   } catch (error) {
-    console.log('Error while calling setConversation api', error.message);
+    console.log("Error while calling setConversation api", error.message);
   }
-}
+};
 export const getConversation = async (data) => {
   try {
     let response = await axios.post(`${url}/conversation/get`, data);
     return response.data;
   } catch (error) {
-    console.log('Error while calling get Conversation api', error.message);
+    console.log("Error while calling get Conversation api", error.message);
   }
-}
+};
 export const newMessage = async (data) => {
   try {
     await axios.post(`${url}/message/add`, data);
   } catch (error) {
-    console.log('Error while calling new message api', error.message);
+    console.log("Error while calling new message api", error.message);
   }
-}
+};
 export const getMessages = async (id) => {
   try {
     let response = await axios.get(`${url}/message/get/${id}`);
-    return response.data
+    return response.data;
   } catch (error) {
-    console.log('Error while calling get message api', error.message);
+    console.log("Error while calling get message api", error.message);
   }
-}
+};
 export const uploadFile = async (data) => {
   try {
     await axios.post(`${url}/file/upload`, data);
   } catch (error) {
-    console.log('Error while calling upload file api', error.message);
+    console.log("Error while calling upload file api", error.message);
   }
-}
+};
